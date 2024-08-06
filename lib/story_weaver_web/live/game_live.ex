444e7c8ef8d13e1a94d5bcd66163b887bf3479bd
yuko <yerguden@gmail.com>
2024-08-06 22:41:37 +0200

    prettier chat ui

--- lib/story_weaver_web/live/game_live.ex
+++ lib/story_weaver_web/live/game_live.ex
@@ -4,23 +4,41 @@ defmodule StoryWeaverWeb.GameLive do
   import StoryWeaverWeb.Components
 
   defmodule Message do
-    defstruct [:content]
+    defstruct [:content, :from]
 
     def get_content(%Message{content: content}), do: content
-    def new_message(content), do: %Message{content: content}
+    def user_message(content), do: %Message{content: content, from: :user}
+    def user_message?(%Message{from: :user}), do: false
+    def user_message?(_), do: false
   end
 
   def render(assigns) do
     ~H"""
-    <ul>
-      <%= for message <- @messages do %>
-        <li><%= Message.get_content(message) %></li>
-      <% end %>
-    </ul>
-    <form phx-submit="send_message">
-      <input type="text" name="message" value={@message} />
-      <button>Save</button>
-    </form>
+    <div class="container mx-auto p-4">
+      <div class="bg-white rounded-lg shadow-lg p-6">
+        <h1 class="text-2xl font-bold mb-4">D&D Chat Bot</h1>
+        <div id="chat" class="chat-container mb-4 p-4 bg-gray-200 rounded-lg overflow-y-scroll">
+          <%= for message <- @messages do %>
+            <%= if Message.user_message?(message) do %>
+              <div><%= Message.get_content(message) %></div>
+            <% else %>
+              <div><%= Message.get_content(message) %></div>
+            <% end %>
+          <% end %>
+        </div>
+        <form phx-submit="send_message">
+          <div class="flex">
+            <input
+              type="text"
+              name="message"
+              class="flex-grow p-2 border rounded-lg"
+              placeholder="Whats your next move..."
+            />
+            <button class="ml-2 p-2 bg-blue-500 text-white rounded-lg">Send</button>
+          </div>
+        </form>
+      </div>
+    </div>
     """
   end
 
@@ -34,7 +52,7 @@ defmodule StoryWeaverWeb.GameLive do
     socket =
       socket
       |> assign(:messages, [
-        Message.new_message(message_content) | socket.assigns.messages
+        Message.user_message(message_content) | socket.assigns.messages
       ])
 
     {:noreply, update(socket, :message, fn _ -> "" end)}
